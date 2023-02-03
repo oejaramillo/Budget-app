7ae6e192e8330d6e260564d448d6ca78a6322848
oejaramillo <oscar.10000@hotmail.com>
2023-02-03 14:38:03 -0300

    definir __init__

--- budget.py
+++ budget.py
@@ -1,19 +1,12 @@
 class Category:
     ledger = []
-    
-    def deposit(self, amount, description=''):
-        self.amount = amount
-        self.description = description
-        self.ledger.append({'amount': self.amount, 'description': self.description})
-           
-        print(self.ledger) 
-
-    def withdraw(self, amount, description=''):
-        self.amount = amount*(-1)
-        self.description = description
-        self.ledger.append({'amount': self.amount, 'description': self.description})        
-        
-        print(self.ledger)
+    name = ''
+ 
+    def check_funds(self, amount):
+        if amount <= self.get_balance():
+            return True
+        else:
+            return False
 
     def get_balance(self):
         total = 0
@@ -22,16 +15,44 @@ class Category:
             amount_values = dic[clave_amount]
             total += amount_values
 
-        print(total)
+        return total
+
+    def deposit(self, amount, description=''):
+        self.amount = amount
+        self.description = description
+        self.ledger.append({'amount': self.amount, 'description': self.description})
+
+    def withdraw(self, amount, description=''):
+        self.amount = amount*(-1)
+        self.description = description
+        
+        if self.check_funds(amount) == True:
+            self.ledger.append({'amount': self.amount, 'description': self.description})        
+            return True
+        else:
+            self.ledger = self.ledger
+            return False
+
+    def transfer(self, amount, category):
+        self.amount = amount
+
+        self.withdraw(self.amount, f'Transfer to {category}')
+        category.deposit(amount, f'Transfer from {self}')
+
+        if self.ledger == self.ledger:
+            return False
+        else:
+            return True
+
+
+
+    
+
 
 
 food = Category()
-food.deposit(215)
-food.deposit(480, 'varios')
-food.withdraw(10)
-food.deposit(98)
-food.deposit(78)
-food.deposit(218)
-food.withdraw(89)
-food.withdraw(100)
-food.get_balance()
+clothing = Category()
+food.deposit(200)
+clothing.deposit(500)
+
+print(food.get_balance())
