547deec1a89f01515d9f9804d8c8db78420eeee1
oejaramillo <oscar.10000@hotmail.com>
2024-10-01 19:40:13 -0400

    database configuration

--- budget_app/budget_app/settings.py
+++ budget_app/budget_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'budget_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget_db',
+        'USER': 'edu',
+        'PASSWORD': 'tqfBKx9eCq7M',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
