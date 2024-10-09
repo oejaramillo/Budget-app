43fcbfec7a1d004843db02001d21e46f0083e172
oejaramillo <oscar.10000@hotmail.com>
2024-10-08 21:34:23 -0400

    restart

--- backend/src/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, current_app, flash
-from app import app
-from models import Transacciones, CuentaPresupuesto, Cuentas, Categorias, Presupuestos, Monedas
-
-@app.route('/')
-def inicio():
-    return render_template('index.html')
