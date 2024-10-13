70ccedce47a01e93c5006f7129546852daf867d0
oejaramillo <oscar.10000@hotmail.com>
2024-10-13 18:32:44 -0400

    testing

--- budget_app/tracker/urls.py
+++ budget_app/tracker/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import CurrencyViewSet, AccountViewSet, BudgetViewSet, CategoryViewSet, TransactionViewSet, AccountBudgetViewSet
 
 router = DefaultRouter()
-router.register(r'currencies', CurrencyViewSet)
-router.register(r'accounts', AccountViewSet)
-router.register(r'budgets', BudgetViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'transactions', TransactionViewSet)
-router.register(r'accounts-budgets', AccountBudgetViewSet)
+router.register(r'currencies', CurrencyViewSet, basename='currency')
+router.register(r'accounts', AccountViewSet, basename='account')
+router.register(r'budgets', BudgetViewSet, basename='budget')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+router.register(r'accounts-budgets', AccountBudgetViewSet, basename='account-budget')
 
 urlpatterns = [
     path('api/v1/', include((router.urls, 'budget_app'))),
