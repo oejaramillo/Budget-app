bd204f6c656f5aba5f6ffe35e009f4130b014130
oejaramillo <oscar.10000@hotmail.com>
2025-06-21 19:05:59 -0400

    frontend initial setup, axios setup and organization, ProtectedRoutes api token acces and refresher implementation for wrapping protected routes, and api component, env and constant for jwt setup

---
+++ frontend/src/api.js
@@ -0,0 +1,22 @@
+// interceptor that automatically add the correct headers via axios to send network requests
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
